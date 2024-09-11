31868b86aeab8afbaee7ffaa037fc574b845ccdf
DanilKasianenko <danil.kasianenko@requestum.com>
2024-09-11 17:19:34 +0300

    feat: first video ( set-up project )

---
+++ ios/Live Activity/Live_ActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Live_ActivityBundle.swift
+//  Live Activity
+//
+//  Created by Данил Касьяненко on 14.08.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Live_ActivityBundle: WidgetBundle {
+    var body: some Widget {
+//        Live_Activity()
+        Live_ActivityLiveActivity()
+    }
+}
